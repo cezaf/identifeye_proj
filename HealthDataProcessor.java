f5059bfe0c557d76453521c2b7ad08f0f1bce0cb
cezaf <117246506+cezaf@users.noreply.github.com>
2023-03-26 23:27:57 -0400

    Final Edits
    Made code comments more concise and made variable names clearer

--- HealthDataProcessor.java
+++ HealthDataProcessor.java
@@ -8,16 +8,15 @@
 
 import java.util.*;
 import java.io.*;
-import java.lang.*;
 
 public class HealthDataProcessor {
     
-    private Map<String, String> patient; // Key-Value Pair: Patient ID - Name 
+    private Map<String, String> patients; // Key-Value Pair: Patient ID - Name 
     private Map<String, HashSet<String>> exams; // Patient ID - Associated Exam IDs
     private Map<String, String> examIdentifier; // Exam ID - Patient ID; assumes that Exam IDs are unique
 
     public HealthDataProcessor(){
-        this.patient = new HashMap<>();
+        this.patients = new HashMap<>();
         this.exams = new HashMap<>();
         this.examIdentifier = new HashMap<>();
     }
@@ -27,21 +26,21 @@ public class HealthDataProcessor {
     // Description: either adds a patient or exam file
     public void addRecord(String[] line){
         if(line[1].equals("PATIENT")){ 
-            // line composition: ADD PATIENT ID NAME1 NAME2
-            if(!this.patient.containsKey(line[2])){ // if patient with this ID does not exist
+            // line composition: ADD PATIENT PATIENT_ID NAME1 NAME2
+            if(!this.patients.containsKey(line[2])){ 
                 int counter = 4;
                 while(counter <= line.length - 1){ // compiles all following text (i.e. name of the patient) into line[3]
                     line[3] += " " + line[counter++];
                 }
-                this.patient.put(line[2],line[3]);
+                this.patients.put(line[2],line[3]);
             }
-        } else { // equals "EXAM"
+        } else { 
             // line composition: ADD EXAM PATIENT_ID EXAM_ID
-            if(this.patient.containsKey(line[2])){ // if patient with this ID does exist
-                if(!this.exams.containsKey(line[2])){
-                    this.exams.put(line[2],new HashSet<>()); // initializes hashset if patient had no previous exams
-                } 
-                if(!this.examIdentifier.containsKey(line[3])){ // if exam with that ID does not exist, add it
+            if(this.patients.containsKey(line[2])){  
+                if(!this.examIdentifier.containsKey(line[3])){ 
+                    if(!this.exams.containsKey(line[2])){
+                        this.exams.put(line[2],new HashSet<>()); // initializes hashset if patient had no previous exams
+                    }
                     this.exams.get(line[2]).add(line[3]);
                     this.examIdentifier.put(line[3],line[2]);
                 }
@@ -52,21 +51,21 @@ public class HealthDataProcessor {
     // Input: String array, composed of different sections of one line in the text file (ex. "DEL", "PATIENT", "123")
     // Output: none, only changes class attributes
     // Description: will delete either patient or exam record
-    public void delRecord(String[] line){
+    public void deleteRecord(String[] line){
         if(line[1].equals("PATIENT")){ 
             // line composition: DEL PATIENT PATIENT_ID
-            if(this.patient.containsKey(line[2])){ // if the patient exists
-                this.patient.remove(line[2]);
+            if(this.patients.containsKey(line[2])){ 
+                this.patients.remove(line[2]);
                 if(this.exams.get(line[2]) != null){ // if the patient had exams
-                    for(String exam : this.exams.get(line[2])){ // removing every exam of the removed patient from examIdentifier
+                    for(String exam : this.exams.get(line[2])){ 
                         this.examIdentifier.remove(exam);
                     }
                     this.exams.remove(line[2]);
                 }
             } 
-        } else { // equals "EXAM" 
+        } else {  
             // line composition: DEL EXAM EXAM_ID
-            if(this.examIdentifier.containsKey(line[2])){ // if the exam exists
+            if(this.examIdentifier.containsKey(line[2])){ 
                 String patientID = this.examIdentifier.get(line[2]); 
                 this.exams.get(patientID).remove(line[2]);
                 this.examIdentifier.remove(line[2]);
@@ -76,12 +75,12 @@ public class HealthDataProcessor {
 
     // Input: none
     // Output: none, only prints statements to terminal
-    // Description: will print output statement to the terminal containing the names, IDs, and exam counts of all patients
+    // Description: will print output statement to the terminal containing the names, IDs, exam counts, and exam lists of all patients
     public void output(){
-        for (Map.Entry<String,String> entry : this.patient.entrySet()){ // loops through "patient" to retrieve data
+        for (Map.Entry<String,String> entry : this.patients.entrySet()){ 
             if(this.exams.get(entry.getKey()) == null){ // if the patient had no exams
                 System.out.println("Name: " + entry.getValue() + ", ID: " + entry.getKey() + ", Exam Count: 0");
-            } else { // if the patient had exams
+            } else { 
                 System.out.println("Name: " + entry.getValue() + ", ID: " + entry.getKey() + ", Exam Count: " + this.exams.get(entry.getKey()).size() + ", Exam List: " + Arrays.toString(this.exams.get(entry.getKey()).toArray()));
             }
         }
@@ -97,8 +96,8 @@ public class HealthDataProcessor {
             String[] line = str.split("\\s+");
             if(line[0].equals("ADD")){
                 processor.addRecord(line);
-            } else { // equals "DEL"
-                processor.delRecord(line);
+            } else { 
+                processor.deleteRecord(line);
             }
         }
         br.close();    
